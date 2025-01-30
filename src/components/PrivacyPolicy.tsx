301ec9cc90e5358bfaf3dddbeca15bcdc6546221
Julio Castro <julito.1998@hotmail.es>
2025-01-29 21:13:29 -0500

    correct prv terms

--- src/components/PrivacyPolicy.tsx
+++ src/components/PrivacyPolicy.tsx
@@ -4,7 +4,7 @@ import MdViewer from "./utils/MdViewer";
 class PrivacyPolicy extends PureComponent {
     render() {
         return (
-            <div className="container mx-auto p-5 max-h-[80vh] overflow-y-auto max-w-3xl mx-auto">
+            <div className="container mx-auto p-5 max-h-[80vh] overflow-y-auto max-w-3xl">
                 <MdViewer url="https://summitexplorerjd.sfo3.digitaloceanspaces.com/privacy&terms/POLITICASDEPRIVACIDAD.md"></MdViewer>
             </div>
         );
