942f6850a6307cc622664b42ff44488cde1570c4
julio <julito.1998@hotmail.es>
2025-02-03 16:48:53 -0500

    add acordion to about

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,42 +1,35 @@
 import { FC } from "react";
 import Carousel from "./utils/Carousel";
 import './AboutMe.css';
+import Accordion from "./utils/Accordion";
 
 const AboutMe: FC = () => {
 
     const slideImgLst: string[] = ['https://via.placeholder.com/800x400', 'https://via.placeholder.com/800x400', 'https://via.placeholder.com/800x400'];
 
     const slideContentLst: JSX.Element[] = [
-        <>
-            <h3>Conoceme</h3>
-            <p>¡Hola! Soy Julio Castro, un ingeniero de software con una profunda pasión por la escalada, el alpinismo y la ciencia. Mi vida se define por el equilibrio entre la innovación tecnológica y la conexión con la naturaleza. Cada línea de código que escribo y cada montaña que escalo son reflejo de mi compromiso con la superación personal y profesional.</p>
-        </>,
-        <>
-            <h3>Mi Trayectoria y Logros</h3>
-            <ul>
+        <Accordion title="Conoceme" classNames={{ header: 'w-full', content: 'flex flex-row flex-wrap justify-center' }}>
+            <p className="m-4 md:w-1/2 w-full">¡Hola! Soy Julio Castro, un ingeniero de software con una profunda pasión por la escalada, el alpinismo y la ciencia. Mi vida se define por el equilibrio entre la innovación tecnológica y la conexión con la naturaleza. Cada línea de código que escribo y cada montaña que escalo son reflejo de mi compromiso con la superación personal y profesional.</p>
+        </Accordion>,
+        <Accordion title="Mi Trayectoria y Logros" classNames={{ header: 'w-full', content: 'flex flex-row flex-wrap justify-center' }}>
+            <ul className="m-4 md:w-1/2 w-full">
                 <li>He trabajado en el desarrollo de soluciones tecnológicas innovadoras, creando herramientas que facilitan la vida de las personas y empoderan a comunidades.</li>
                 <li>Mi pasión por la montaña me ha llevado a explorar paisajes increíbles, conquistar cimas desafiantes y adquirir lecciones invaluables sobre resiliencia y trabajo en equipo.</li>
                 <li>Persigo el sueño de obtener un doctorado en física teórica, combinando mi curiosidad científica con mi amor por el aprendizaje constante.</li>
             </ul>
-        </>,
-        <>
-            <h3>Mi Filosofía de Vida</h3>
-            <p>Creo firmemente que cada desafío, ya sea en el mundo digital o en la inmensidad de las montañas, es una oportunidad para aprender, crecer y transformar. La clave está en no detenerse nunca, en buscar siempre esa "cima" que nos motiva a ser mejores.</p>
-        </>,
-        <>
-            <h3>Misión y Visión</h3>
-            <ul>
+        </Accordion>,
+        <Accordion title="Mi Filosofía de Vida" classNames={{ header: 'w-full', content: 'flex flex-row flex-wrap justify-center' }}>
+            <p className="m-4 md:w-1/2 w-full">Creo firmemente que cada desafío, ya sea en el mundo digital o en la inmensidad de las montañas, es una oportunidad para aprender, crecer y transformar. La clave está en no detenerse nunca, en buscar siempre esa "cima" que nos motiva a ser mejores.</p>
+        </Accordion>,
+        <Accordion title="Misión y Visión" classNames={{ header: 'w-full', content: 'flex flex-row flex-wrap justify-center' }}>
+            <ul className="m-4 md:w-1/2 w-full">
                 <li>Mi misión es inspirar a otros a alcanzar sus metas más altas, ya sea mediante tecnología innovadora, aventuras únicas o compartiendo conocimientos que conecten a las personas con la ciencia y la naturaleza.</li>
                 <li>Mi visión es ser un referente en la unión entre tecnología, ciencia y aventura, demostrando que la curiosidad y la creatividad no tienen límites.</li>
             </ul>
-        </>,
-        <>
-            <h3>Un Puente entre Naturaleza y Tecnología</h3>
-            <p>
-                En SummitExplorer JD combino lo mejor de dos mundos: las alturas de las montañas con la profundidad del conocimiento digital. Mi objetivo es crear una comunidad donde la tecnología y el amor por la naturaleza se complementen, inspirando a más personas a superar sus propios límites.
-            </p>
-            <p>Cada proyecto es una nueva expedición, cada interacción es una oportunidad de conectar. Te invito a formar parte de este viaje, donde juntos podemos alcanzar cumbres extraordinarias.</p>
-        </>
+        </Accordion>,
+        <Accordion title="Un Puente entre Naturaleza y Tecnología" classNames={{ header: 'w-full', content: 'flex flex-row flex-wrap justify-center' }}>
+            <p className="m-4 md:w-1/2 w-full">En SummitExplorer JD combino lo mejor de dos mundos: las alturas de las montañas con la profundidad del conocimiento digital. Mi objetivo es crear una comunidad donde la tecnología y el amor por la naturaleza se complementen, inspirando a más personas a superar sus propios límites.</p>
+        </Accordion>
     ];
 
     return (
@@ -48,7 +41,10 @@ const AboutMe: FC = () => {
                 {slideContentLst.map((content) => (<div>{content}</div>))}
             </article>
             <article className="h-[70vh]">
-                <Carousel timeInterval={3000} slideImgLst={slideImgLst}></Carousel>
+                <Carousel timeInterval={3000} slideImgLst={slideImgLst} classNames={{ content: 'hidden' }}></Carousel>
+            </article>
+            <article className="flex flex-row flex-wrap justify-center text-center">
+                <p className="m-4 md:w-2/3 w-full">Cada proyecto es una nueva expedición, cada interacción es una oportunidad de conectar. Te invito a formar parte de este viaje, donde juntos podemos alcanzar cumbres extraordinarias.</p>
             </article>
         </section>
     );
