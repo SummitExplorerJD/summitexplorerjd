12f15ac47e035f41da86ddf5c1164f33f5b2373e
Julio Castro <julito.1998@hotmail.es>
2025-04-24 00:48:31 -0500

    correct s

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -50,9 +50,9 @@ const Projects: FC = () => {
     };
 
     return (
-        <section id="Proyectos" className="py-20 bg-[var(--neutral-1o40-smjd)]">
+        <section id="Proyectos" className="pt-10 pb-15 bg-[var(--neutral-1o40-smjd)]">
             <div className="container mx-auto px-4">
-                <h1 className="text-4xl font-bold text-center mb-12">Proyectos</h1>
+                <h2 className="text-[var(--primary-smjd)] text-2xl font-bold text-center mb-12">Proyectos</h2>
 
                 <motion.div
                     className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"
